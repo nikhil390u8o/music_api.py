f4c80ce8b0cadb40a7c9e42e5903148032526150
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2025-11-12 19:53:33 +0530

    Update music_api.py

--- music_api.py
+++ music_api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import yt_dlp
+import os
 
 app = Flask(__name__)
 
@@ -20,4 +21,5 @@ def song():
         })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=1470)
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
